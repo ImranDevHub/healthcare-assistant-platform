012600c0c5485fffaa7bf49ac00dcf7a2c4fdb1e
Imran Muhammad <imranwebdevelop@gmail.com>
2025-10-06 10:42:49 +0300

    feat: add initial setup for Medical Chatbot with dependencies and configuration

---
+++ chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Final Year Project',
+    author_email= 'imran.4678m@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
